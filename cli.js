4e58e6b62b44a76800634a243f7fec3be6c006ac
hjorthjort <rikard.hjort@gmail.com>
2019-08-05 18:17:37 +0200

    Nicer printing and robustness

--- cli.js
+++ cli.js
@@ -2,35 +2,57 @@
 
 rq = require("request");
 fs = require("fs");
+printf = require("printf");
 
 const API = "http://api.ethplorer.io/";
 const KEY = "?apiKey=freekey";
 
 
-
-const printResponse = (err, resp, body) => {
+const formatResponse = (err, resp, body) => {
+    if (err) {
+        return err["message"];
+    }
     let res = JSON.parse(body);
-    let outputArr = [];
-    outputArr.push("ETH: " + res["ETH"]["balance"]);
+    if (res["error"]) {
+        return res["error"]["message"];
+    }
+    let tokensArr = [];
+    tokensArr.push({"key": "ETH", "bal": res["ETH"]["balance"]});
 
     let tokens = res["tokens"];
     if (tokens) {
         for (let i = 0; i < tokens.length; i++) {
             let tok = tokens[i];
-            let name = tok["tokenInfo"]["name"];
+            let name = tok["tokenInfo"]["symbol"];
             let bal = tok["balance"];
-            outputArr.push(name + ": " + bal);
+            tokensArr.push({"key": name, "bal": bal});
         }
     }
-    console.log(outputArr.join("\n"));
+    let lhsLen = Math.max(...tokensArr.map(entry => entry["key"].length)) + 2;
+    let outputArr = tokensArr.map(entry => printf("\x1b[1m%"+lhsLen+"s: \x1b[0m%s", entry["key"], entry["bal"]));
+    return outputArr.join("\n");
+};
+
+
+const printPretty = (addr) => (err, resp, body) => {
+    console.log(" \x1b[1m\x1b[4m" + addr); // Make bright.
+    console.log("\x1b[0m\n" + formatResponse(err, resp, body) + "\n"); // Reset color.
 };
 
 
 // Main.
 
-fs.readFile("address.txt", (err, addr) => {
-    addr = "" + addr;
-    addr = addr.trim();
-    let request = API + "getAddressInfo/" + addr + KEY;
-    rq.get(API + "getAddressInfo/" + addr  + KEY, printResponse);
+fs.readFile("address.txt", (err, addrs) => {
+    if (err) {
+        console.log(err);
+        return ;
+    }
+    addrs = "" + addrs;
+    addrs = addrs.trim();
+    addrs = addrs.split("\n");
+    for (let i = 0; i < addrs.length; i++) {
+        let addr = addrs[i];
+        let request = API + "getAddressInfo/" + addr + KEY;
+        rq.get(API + "getAddressInfo/" + addr  + KEY, printPretty(addr));
+    }
 });
